649ca2b4e4e72f019e94525ac1a422dbadd597a1
Wagner Mello <wagnermellow2k@gmail.com>
2023-05-10 17:21:59 -0300

    main: create a singleton to guest form repository

--- app/src/main/java/com/example/convidados/repository/GuestFormRepository.kt
+++ app/src/main/java/com/example/convidados/repository/GuestFormRepository.kt
@@ -1,4 +1,20 @@
 package com.example.convidados.repository
 
 class GuestFormRepository {
+
+    //Singleton
+    companion object {
+        private lateinit var repository: GuestFormRepository
+
+        fun getInstance(): GuestFormRepository {
+            if (!::repository.isInitialized) {
+                repository = GuestFormRepository()
+            }
+            return repository
+        }
+    }
+
+    fun save() {
+
+    }
 }
