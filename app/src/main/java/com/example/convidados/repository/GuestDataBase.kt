6025dd234c20fbf48864c2df2a08862fe2591256
Wagner Mello <wagnermellow2k@gmail.com>
2023-05-12 22:48:23 -0300

    main: setting database with ROOM

--- app/src/main/java/com/example/convidados/repository/GuestDataBase.kt
+++ app/src/main/java/com/example/convidados/repository/GuestDataBase.kt
@@ -1,27 +1,38 @@
 package com.example.convidados.repository
 
 import android.content.Context
-import android.database.sqlite.SQLiteDatabase
-import android.database.sqlite.SQLiteOpenHelper
-import com.example.convidados.constants.DataBaseConstants
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.example.convidados.model.GuestModel
 
-class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+@Database(entities = [GuestModel::class], version = 1)
+abstract class GuestDataBase : RoomDatabase() {
 
     companion object {
-        private const val NAME = "guestdb"
-        private const val VERSION = 1
-    }
+        private lateinit var INSTANCE: GuestDataBase
 
-    override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL(
-            "create table " + DataBaseConstants.GUEST.TABLE_NAME + " (" +
-                    DataBaseConstants.GUEST.COLUMNS.ID + " integer primary key autoincrement, " +
-                    DataBaseConstants.GUEST.COLUMNS.NAME + " text, " +
-                    DataBaseConstants.GUEST.COLUMNS.PRESENCE + " integer);"
-        )
-    }
+        fun getDataBase(context: Context): GuestDataBase {
+            if (!::INSTANCE.isInitialized) {
+                // used synchronized to ensure thread's running
+                synchronized(GuestDataBase::class) {
+                    INSTANCE =
+                        Room.databaseBuilder(context, GuestDataBase::class.java, "guestdb")
+                            .addMigrations(MIGRATION_1_2)
+                            .allowMainThreadQueries()
+                            .build()
+                }
+            }
+            return INSTANCE
+        }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                TODO("Not yet implemented")
+            }
 
+        }
     }
 }
