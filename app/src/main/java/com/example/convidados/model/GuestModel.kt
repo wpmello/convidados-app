c9d0692a2dfe1c500ea43cceb278c85dfcc5bc6b
Wagner Mello <wagnermellow2k@gmail.com>
2023-05-12 22:25:27 -0300

    main: setting ROOM

--- app/src/main/java/com/example/convidados/model/GuestModel.kt
+++ app/src/main/java/com/example/convidados/model/GuestModel.kt
@@ -1,7 +1,19 @@
 package com.example.convidados.model
 
-data class GuestModel(
-    val id: Int,
-    var name: String,
-    var isPresent: Boolean
-)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Guest")
+class GuestModel {
+
+    @ColumnInfo
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+
+    @ColumnInfo
+    var name: String = ""
+
+    @ColumnInfo
+    var isPresent: Boolean = false
+}
